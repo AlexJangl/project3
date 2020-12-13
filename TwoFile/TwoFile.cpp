13ddb704b0d496abe2cd326e96aa56a6e6a1caa8
Alexander <jangl@i.ua>
2020-12-14 00:25:31 +0200

    finish project

--- TwoFile/TwoFile.cpp
+++ TwoFile/TwoFile.cpp
@@ -1,8 +1,37 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+#include <windows.h>
+#include <fstream>
 
-int main()
+int main(int argc, char** argv)
 {
+    STARTUPINFO si = {};
+    PROCESS_INFORMATION pi = {};
+    if (!CreateProcess(L"C:\\GitProject\\project3\\FirstFile\\Debug\\FirstFile.exe", 
+        0,        
+        NULL,           
+        NULL,           
+        FALSE,          
+        0,              
+        NULL,           
+        NULL,            
+        &si,            
+        &pi)           
+        )
+    {
+        std::cout<<("CreateProcess failed (%d).\n", GetLastError());
+        return 0;
+    }
+    Sleep(10);
+    char buff[50]; 
+    std::ifstream fin("file.txt"); 
+
+    if (!fin.is_open()) 
+        std::cout << "Файл не может быть открыт!\n";
+    else
+    {
+        fin.getline(buff, 50);
+        fin.close();
+        std::cout << buff << std::endl;
+    }
     
 }
-
